8d46f66f5a869ad411d5f7c2ee5b76dcf1b80295
Varun <varunpurohit76@gmail.com>
2017-07-08 19:29:29 +0530

    regression template

--- udemy_a_z/3_classification/template/classification_template.py
+++ udemy_a_z/3_classification/template/classification_template.py
@@ -25,9 +25,7 @@ x_train = sc_x.fit_transform(x_train)
 x_test = sc_x.transform(x_test)
 
 # model
-from sklearn.linear_model import LogisticRegression
-classifier = LogisticRegression(random_state = 42)
-classifier.fit(x_train, y_train)
+
 
 # predict
 y_pred = classifier.predict(x_test)
@@ -39,17 +37,6 @@ cm = confusion_matrix(y_test, y_pred)
 # plot
 from matplotlib.colors import ListedColormap
 x_set, y_set = x_train, y_train
-x1, x2 = np.meshgrid(np.arange(start = x_set[:, 0].min() - 1, stop = x_set[:, 0].max() + 1, step = 0.01),
-                     np.arange(start = x_set[:, 0].min() - 1, stop = x_set[:, 0].max() + 1, step = 0.01))
-plt.contour(x1, x2, classifier.predict(np.array([x1.ravel(), x2.ravel()]).T).reshape(x1.shape), alpha = 0.75, cmap = ListedColormap(('red', 'green')))
-plt.xlim(x1.min(), x1.max())
-plt.ylim(x2.min(), x2.max())
-for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(x_set[y_set == j, 0], x_set[y_set == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
-plt.legend()
-plt.show()
-
-x_set, y_set = x_test, y_test
 x1, x2 = np.meshgrid(np.arange(start = x_set[:, 0].min() - 1, stop = x_set[:, 0].max() + 1, step = 0.01),
                      np.arange(start = x_set[:, 0].min() - 1, stop = x_set[:, 0].max() + 1, step = 0.01))
 plt.contour(x1, x2, classifier.predict(np.array([x1.ravel(), x2.ravel()]).T).reshape(x1.shape), alpha = 0.75, cmap = ListedColormap(('red', 'green')))
